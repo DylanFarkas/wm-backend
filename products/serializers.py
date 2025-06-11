68eefd68641ab11a0c58475d0e96fd6b91a4df9d
Dylan Farkas <dylan.farkas@correounivalle.edu.co>
2025-06-11 13:16:14 -0500

    feat: Base of products made

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,37 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Product, Productvariant, ProductImage, ProductSize
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+        
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image']
+        
+class ProductSizeSerializer(serializers.ModelSerializer):
+    variant_detail = serializers.StringRelatedField(source='variant', read_only=True)
+    class Meta:
+        model = ProductSize
+        fields = ['id', 'size','variant', 'variant_detail', 'stock']
+        
+class ProductVariantSerializer(serializers.ModelSerializer):
+    sizes = ProductSizeSerializer(many=True, read_only=True)
+    images = ProductImageSerializer(many=True, read_only=True)
+    class Meta:
+        model = Productvariant
+        fields = ['id', 'color', 'product', 'sizes', 'images']   
+          
+class ProductSerializer(serializers.ModelSerializer):
+    variants = ProductVariantSerializer(many=True, read_only=True)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())  # o usa `PrimaryKeyRelatedField` si prefieres
+    category_detail = serializers.StringRelatedField(source='category', read_only=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            'id', 'name', 'description', 'discount', 'price',
+            'is_active', 'created_at', 'category', 'category_detail', 'variants'
+        ]    
--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,11 @@ from rest_framework import routers
 from products import views
 
 router = routers.DefaultRouter()
-router.register(r'categories', views.CategoryView, 'categories')
+router.register(r'categories', views.CategoryViewSet, 'categories')
+router.register(r'products', views.ProductViewSet, 'products')
+router.register(r'variants', views.ProductVariantViewSet, 'variants')
+router.register(r'sizes', views.ProductSizeViewSet, 'sizes')
+router.register(r'images', views.ProductImageViewSet, 'images')
 
 urlpatterns = [
     path('api/', include(router.urls)),
