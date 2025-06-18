84869753643248cf92834f8e062c45ce325ea62d
Dylan Farkas <dylan.farkas@correounivalle.edu.co>
2025-06-17 20:37:32 -0500

    new categories

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20, unique=True)
     description = models.TextField(blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='subcategories', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
